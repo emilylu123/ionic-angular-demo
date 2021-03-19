d66ecb3ee955d248768d62dce6ce811768072733
Yingyao Lu <emilylu123@gmail.com>
2021-03-19 22:01:23 +1030

    Extracting and displaying route param data

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl:
+        'https://www.daringgourmet.com/wp-content/uploads/2014/05/Swabian-Potato-Salad-1.jpg',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad'],
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl:
+        'https://i0.wp.com/sugarspiceslife.com/wp-content/uploads/2019/03/Greek-Meat-Sauce-with-Spaghetti-5.jpg?resize=683%2C1024&ssl=1',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes'],
+    },
+  ];
+
+  constructor() {}
+
+  public getAllRecipes(): Recipe[] {
+    return [...this.recipes];
+  }
+
+  public getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find((recipe) => {
+        return recipe.id === recipeId;
+      }),
+    };
+  }
+}
